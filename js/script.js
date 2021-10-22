121c19854eee325b0793d7a3ef07d5437a1d90c4
Moses Samuel <devparse.official@gmail.com>
2021-10-22 15:09:08 +0100

    New updates

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@ function togglenav(x){
     x.classList.toggle("fa-times");
     document.querySelector(".mobile").classList.toggle("active")
 }
+
+const sr = ScrollReveal({
+    origin: 'bottom',
+    distance: '30px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal(`.account__data, .home__img,
+            .about__data, .about__img,
+            .services__content, .menu__content,
+            .app__data, .app__img,
+            .contact__data, .contact__button,
+            .footer__content`, {
+    interval: 200
+})
